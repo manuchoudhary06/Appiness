766fb296eb60c92e922b37e66510104eb0d849ee
Manu Choudhary <manu.choudhary@grazitti.local>
2022-01-02 20:16:23 +0530

    captcha

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // If reCAPTCHA is still loading, grecaptcha will be undefined.
 grecaptcha.ready(function () {
   grecaptcha.render("container", {
-    sitekey: "6LdDIuYdAAAAAM8KBC1nIuLddsMZSugfuklWqfYo",
+    sitekey: "6LdDIuYdAAAAAP_2go-yMawVhQZl_NaV8N37-PNi",
   });
 });
